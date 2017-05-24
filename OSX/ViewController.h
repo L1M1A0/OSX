13c4bd5eb1fdfe4cf42a61a7f5f77702ae3fab84
李振彪 <vae@macbiaoair.local>
2017-05-24 17:20:17 +0800

    Initial Commit

---
+++ OSX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OSX
+//
+//  Created by 李振彪 on 2017/5/24.
+//  Copyright © 2017年 李振彪. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ OSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  OSX
+//
+//  Created by 李振彪 on 2017/5/24.
+//  Copyright © 2017年 李振彪. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
